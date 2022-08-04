ecfb1f41c4733d6390a671d751943d483d4e9945
Ekko0701 <djekko0701@gmail.com>
2022-08-05 00:59:22 +0900

    BOJ 10808 알파벳 개수

---
+++ Algorithm Swift/BaekJoon/10808_알파벳 개수.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  Algorithm Swift
+//  Baekjoon
+//  10808 - 알파벳 개수
+
+import Foundation
+
+let input = readLine()!
+var result: [Int] = Array(repeating: 0, count: 26)
+
+for char in input {
+    result[Int(char.asciiValue! - 97)] += 1
+}
+
+print(result.map{ String($0) }.joined(separator: " "))
+
