8922f46853cfbf5e9f343d1219cef3ae0575df30
Ekko0701 <djekko0701@gmail.com>
2022-08-06 12:29:18 +0900

    Programmers 신고 결과 받기

---
+++ Programmers/신고결과받기.swift
@@ -0,0 +1,38 @@
+//
+//  main.swift
+//  Algorithm Swift
+//  Programmers
+//  신고 결과 받기
+
+import Foundation
+
+func solution(_ id_list:[String], _ report:[String], _ k:Int) -> [Int] {
+    var result: [Int] = Array(repeating: 0, count: id_list.count)
+    var reported: [String: [String]] = [:]
+    var banned: [String] = []
+    
+    id_list.forEach { id in
+        reported[id] = []
+    }
+    
+    for i in Set(report) {
+        let splitId = i.split(separator: " ").map { String($0) }
+        reported[splitId[1]]?.append(splitId[0])
+    }
+    
+    for (key, value) in reported {
+        if value.count >= k {
+            banned.append(key)
+        }
+    }
+    
+    for i in banned {
+        reported[i]?.forEach{ id in
+            result[id_list.firstIndex(of: id)!] += 1
+        }
+    }
+    
+    return result
+}
+
+print(solution(["muzi", "frodo", "apeach", "neo"], ["muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"], 2))
